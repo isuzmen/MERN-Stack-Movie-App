6cef04ab6e253467a8d9c1af3bf4c7ac97e07053
İlyas SÜZMEN <73710864+isuzmen@users.noreply.github.com>
2025-04-22 12:56:51 +0300

    The getAllUsers function retrieves the list of all users using User.find({}). The getUserProfile function returns the profile information of the authenticated user, where req.user._id is used to query the user's data in the database. If the user is found, the profile information is returned as JSON; if not, a 404 error is thrown with a message. The updateUserProfile function allows the authenticated user to update their profile, including username, email, or password. If the password is being changed, it is hashed securely using bcrypt. The updated user data is returned as JSON. All these operations are defined in the userRoutes for the / and /profile routes. Both routes require user authentication, and the / route also checks for admin authorization.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,27 @@ import {
   createUser,
   loginUser,
   logoutUser,
+  getAllUsers,
+  getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
+
+import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(createUser);
+router
+  .route('/')
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+
 router.post('/auth', loginUser);
+
 router.post('/logout', logoutUser);
+
+router
+  .route('/profile')
+  .get(authenticate, getUserProfile)
+  .put(authenticate, updateUserProfile);
 
 export default router;
